764339e7c4af3c3eeac75fd2aafad05e1c550887
botmakin <119750664+botmakin@users.noreply.github.com>
2023-01-02 22:11:20 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -17,7 +17,7 @@ for name in files:
 
 #Don't be a thief 
 print("Successfully deployed!")
-print("By aarthur_dayne • Movies Halt")
+print("By Arthur Dayne • Movies Halt")
 
 if __name__ == "__main__":
     bot.run_until_disconnected()
